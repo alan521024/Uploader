b8346df61b9314d74d1bc759cdc79f6fb65b673a
孙兵 <carl900@live.com>
2017-11-08 23:13:01 +0800

    注册界面

--- DoubleX.Upload/Register.xaml.cs
+++ DoubleX.Upload/Register.xaml.cs
@@ -24,6 +24,7 @@ using Newtonsoft.Json.Linq;
 using DoubleX.Infrastructure.Utility;
 using DoubleXUI.Controls;
 using System.Diagnostics;
+using System.Drawing;
 
 namespace DoubleX.Upload
 {
@@ -32,6 +33,15 @@ namespace DoubleX.Upload
     /// </summary>
     public partial class Register : DxWindow
     {
+        public Register()
+        {
+            InitializeComponent();
+            ViewPageInit();
+            LoadingLicense();
+        }
+
+        #region 私有变量
+
         /// <summary>
         /// 授权信息
         /// </summary>
@@ -42,24 +52,75 @@ namespace DoubleX.Upload
         /// </summary>
         private LicenseStatModel licenseStatModel { get; set; }
 
-        public Register()
-        {
-            InitializeComponent();
+        /// <summary>
+        /// 验证码
+        /// </summary>
+        private string CaptchaCode { get; set; }
+        
+        /// <summary>
+        /// 视图类型
+        /// </summary>
+        private string ViewType { get; set; }
 
-            //现有授权信息
+        #endregion
+
+        #region 辅助方法
+
+        protected void ViewPageInit()
+        {
+            ViewType = "buy";
+            BindCaptchaCode();
+        }
+
+        /// <summary>
+        /// 加载现有授权信息
+        /// </summary>
+        protected void LoadingLicense()
+        {
             var licPath = string.Format("{0}/data/license.key", AppDomain.CurrentDomain.BaseDirectory).ToLower();
             licenseFileModel = AppHelper.LicenseFileGet(licPath);
             licenseStatModel = AppHelper.LicenseStatGet(licenseFileModel, isAddSelf: false);
+        }
 
-            txtEmail.Text = licenseFileModel.Email;
-            txtMac.Text = MacHelper.GetMacAddress();
-            txtCPU.Text = Win32Helper.GetCpuID();
-            if (!licenseFileModel.IsTrial)
+        protected void BindCaptchaCode()
+        {
+            ControlUtil.ExcuteAction(this, () =>
             {
-                txtPath.Text = "license.key";
+                CaptchaCode = StringRandHelper.Number(4);
+                imgCaptcha.Source = CaptchaHelper.CreateBitmapImage(CaptchaCode);
+            });
+        }
+
+        #endregion
+
+        private void imgCaptcha_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            BindCaptchaCode();
+        }
+
+        private void swCaptch_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 1)
+            {
+                if (ViewType == "buy")
+                {
+                    ViewType = "import";
+                    grdBuy.Visibility = Visibility.Collapsed;
+                    grdImport.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    ViewType = "buy";
+                    grdBuy.Visibility = Visibility.Visible;
+                    grdImport.Visibility = Visibility.Collapsed;
+                }
             }
         }
 
+        private void btnBuy_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
         private void btnOpenFileDialog_Click(object sender, RoutedEventArgs e)
         {
             Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
@@ -69,52 +130,6 @@ namespace DoubleX.Upload
             if (result == true)
             {
                 txtPath.Text = dlg.FileName;
-            }
-        }
-
-        private void btnRegister_Click(object sender, RoutedEventArgs e)
-        {
-            if (string.IsNullOrWhiteSpace(txtEmail.Text))
-            {
-                ControlUtil.ShowMsg("请输入邮箱地址");
-                return;
-            }
-            if (string.IsNullOrWhiteSpace(txtPath.Text))
-            {
-                ControlUtil.ShowMsg("请选择授权文件");
-                return;
-            }
-
-            string email = txtEmail.Text.Trim(), mac = MacHelper.GetMacAddress(), cpu = Win32Helper.GetCpuID();
-            var licPath = txtPath.Text.Trim();
-
-            try
-            {
-                licenseFileModel = AppHelper.LicenseFileGet(licPath);
-                licenseStatModel = AppHelper.LicenseStatGet(licenseFileModel, isAddSelf: false);
-                licenseStatModel.Identification = licenseFileModel.Email;
-
-                if (AppHelper.LicenseVerify(licenseFileModel, licenseStatModel))
-                {
-                    AppHelper.LicenseStatReset(licenseFileModel);
-                }
-                //替换文件
-                var destPath = string.Format("{0}/data/license.key", AppDomain.CurrentDomain.BaseDirectory).ToLower();
-                File.Copy(licPath, destPath, true);
-                if (MessageBox.Show("授权成功，重启程序生效", "授权提示", MessageBoxButton.OK, MessageBoxImage.Question) == MessageBoxResult.OK)
-                {
-                    System.Windows.Forms.Application.Restart();
-                    Application.Current.Shutdown();
-                    return;
-                }
-            }
-            catch (LicenseException ex)
-            {
-                ControlUtil.ShowMsg(ExceptionHelper.GetMessage(ex), "授权提示", icon: MessageBoxImage.Error);
-            }
-            catch (Exception ex)
-            {
-                ControlUtil.ShowMsg(ExceptionHelper.GetMessage(ex), "授权提示", icon: MessageBoxImage.Error);
             }
         }
     }
